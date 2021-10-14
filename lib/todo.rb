56415107862052a5f9d6f4331f632fc490102a6e
Nick Kelley <nkelley@ltvco.com>
2021-10-14 11:04:01 -0400

    Fix sorting because I broke it

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,22 +1,21 @@
 require 'fileutils'
-module ToDo
-  module_function
 
+module ToDo
   TODO_FILE = File.expand_path('~/.todo')
 
-  def init
+  def self.init
     raise "#{TODO_FILE} already exists" if File.exist?(TODO_FILE)
 
     FileUtils.touch(TODO_FILE)
   end
 
-  def list_top(count)
+  def self.list_top(count)
     File.open(TODO_FILE, 'r') do |f|
       count.times { puts f.readline }
     end
   end
 
-  def add
+  def self.add
     print 'Enter todo: '
     todo = readline
 
@@ -25,20 +24,11 @@ module ToDo
     end
   end
 
-  def sort
-    items = File.readlines(TODO_FILE)
-    items.sort! { |a, b| compare_todo(a, b) }
-
-    File.open(TODO_FILE, 'w') do |f|
-      items.each { |i| f.puts i }
-    end
-  end
-
-  def top
+  def self.top
     File.open(TODO_FILE, 'r') { |f| puts f.readline }
   end
 
-  def pop
+  def self.pop
     items = File.readlines(TODO_FILE)
 
     removed = items.shift
@@ -50,10 +40,45 @@ module ToDo
     end
   end
 
-  def compare_todo(a, b)
-    @compare_memo ||= {}
-    comparison = [a, b].sort
-    @compare_memo[comparison] ||= begin
+  def self.sort
+    items = File.readlines(TODO_FILE)
+    sorter = Sorter.new
+    items.sort! { |a, b| sorter.compare(a, b) }
+
+    File.open(TODO_FILE, 'w') do |f|
+      items.each { |i| f.puts i }
+    end
+  end
+
+  class Sorter
+    def initialize
+      @hierarchy = {}
+    end
+
+    def compare(a, b)
+      find_transitive!(a, b)
+      return 1 if @hierarchy[a]&.include? b
+      return -1 if @hierarchy[b]&.include? a
+
+      result = ask_for_higher(a, b)
+
+      if result.positive?
+        insert_hierarchy(a, b)
+      else
+        insert_hierarchy(b, a)
+      end
+
+      result
+    end
+
+    private
+
+    def insert_hierarchy(higher, lower)
+      @hierarchy[higher] ||= []
+      @hierarchy[higher] << lower unless @hierarchy[higher].include? lower
+    end
+
+    def ask_for_higher(a, b)
       chosen = false
 
       until chosen
@@ -65,7 +90,19 @@ module ToDo
         chosen = [1, 2].include? choice
       end
 
-      chosen == 1 ? 1 : -1
+      choice == 1 ? -1 : 1
+    end
+
+    def find_transitive!(a, b)
+      insert_hierarchy(a, b) if downward_path_exists?(a, b)
+      insert_hierarchy(b, a) if downward_path_exists?(b, a)
+    end
+
+    def downward_path_exists?(higher, lower)
+      return false unless @hierarchy[higher]
+      return true if @hierarchy[higher].include?(lower)
+
+      @hierarchy[higher].any? { |i| downward_path_exists?(i, lower) }
     end
   end
 end
